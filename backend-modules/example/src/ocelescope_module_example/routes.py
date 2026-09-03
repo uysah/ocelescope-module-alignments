156a42262924d50061f648b97eadcbf3ec5db3df
Uy Sa Huynh <uysahuynh@gmail.com>
2026-09-03 14:35:41 +0200

    Resource Selector

--- backend-modules/example/src/ocelescope_module_example/routes.py
+++ backend-modules/example/src/ocelescope_module_example/routes.py
@@ -4,17 +4,20 @@ from ocelescope_module_example.models.alignment import AlignmentsResponse
 from ocelescope_backend.app.dependencies import ApiSession, ApiOcel
 from ocelescope_module_example.utils.alignments import compute_alignments
 from ocelescope import PetriNet
-from typing import Tuple
-
+from typing import cast
 router = APIRouter()
 
 
 class HelloResponse(BaseModel):
     message: str
 
+
 @router.get("/{ocel_id}/alignments", operation_id="get_alignments")
-def get_alignments(ocel:ApiOcel, object_type:str) -> AlignmentsResponse:
-    return compute_alignments(ocel,object_type)
+def get_alignments(
+    ocel: ApiOcel, session: ApiSession, object_type: str, resource_id: str | None = None
+) -> AlignmentsResponse:
+    petri_net = cast(PetriNet,session.get_resource(resource_id)) if resource_id else None
+    return compute_alignments(ocel, object_type)
 
 
 @router.get("/{ocel_id}/objects/types", operation_id="objectTypes")
